4d1cba76d73c45f77c6da65e7c58efd04f60b39c
Ted M <practice@example.com>
2020-06-16 13:38:59 -0400

    Practiced with book exercises

--- src/org/launchcode/java/demos/lsn1datatypes/Message.java
+++ src/org/launchcode/java/demos/lsn1datatypes/Message.java
@@ -8,6 +8,8 @@ public class Message {
             return "¡Hola, Mundo!";
         } else if (lang.equals("fr")) {
             return "Bonjour, le monde!";
+        } else if (lang.equals("gr")) {
+            return "Γειά σου Κόσμε!";
         } else {
             return "Hello, World!";
         }
