86beadfe46904a6f5cdf908f049301e34650730a
Ted M <practice@example.com>
2020-06-17 15:20:55 -0400

    Completed class 1 studio

---
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -0,0 +1,31 @@
+package org.launchcode.java.studios.areaofacircle;
+
+import java.util.Scanner;
+
+public class Area {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Double radius = null;
+        System.out.println("Enter the radius of the circle:");
+
+        if (input.hasNextDouble()){
+            radius = input.nextDouble();
+        } else {
+            System.out.println("ERROR: Not a valid entry.");
+            return;
+        }
+
+        while (radius <= 0) {
+            System.out.println("Enter a number greater than 0:");
+            if (input.hasNextDouble()){
+                radius = input.nextDouble();
+            } else {
+                System.out.println("ERROR: Not a valid entry.");
+                return;
+            }
+        }
+
+        input.close();
+        System.out.println("The area of the circle is: " + Circle.getArea(radius));
+    }
+}
